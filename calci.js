1448fe3e5683743ef64b412ac1b66d4477c72de7
suhasbadwaik <156404460+suhasbadwaik@users.noreply.github.com>
2024-01-12 12:37:54 +0530

    Add files via upload

---
+++ calci.js
@@ -0,0 +1,34 @@
+const input = document.getElementById('inputtext');
+const buttons = document.querySelectorAll('button');
+
+function calculate(expression) {
+    console.log(expression);
+    console.log(typeof(expression));
+    try {
+        return new Function('return ' + expression)();
+    } catch (error) {
+        return 'Malformed Operation';
+    }
+}
+
+
+function operation(buttonValue) {
+    if (buttonValue === 'C') {
+        input.value = '';
+    } else if (buttonValue === 'DEL') {
+        input.value = input.value.slice(0, -1);
+    } else if (buttonValue === '=') {
+        input.value = calculate(input.value);
+    } else if (buttonValue === '%') {
+        input.value = calculate(input.value/100); 
+    } else {
+        input.value += buttonValue;
+    }
+}
+
+buttons.forEach(button => {
+    let buttonValue = button.innerText;
+    button.addEventListener('click', function () {
+        operation(buttonValue);
+    });
+});
